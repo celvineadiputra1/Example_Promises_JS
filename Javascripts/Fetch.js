6126e58fc5af1973f6885b406f825132f46c97fd
celvineadiputra <celvineadiputra@mhs.mdp.ac.id>
2020-07-18 17:53:35 +0700

    update UI

--- Javascripts/Fetch.js
+++ Javascripts/Fetch.js
@@ -1,16 +1,22 @@
 fetch("https://randomuser.me/api/")
     .then((res) => res.json())
     .then((data) => {
+        console.log(data);
         setImage(data["results"][0]["picture"]["large"]);
-        setName(data["results"][0]["name"]["first"]);
+        setName(
+            data["results"][0]["name"]["first"],
+            data["results"][0]["email"]
+        );
     });
 
 const setImage = (url) => {
     const image = document.getElementById("image");
     image.setAttribute("src", url);
+    image.setAttribute("alt", url);
 };
 
-const setName = (nameRandom = "Example") => {
+const setName = (nameRandom = "Example", email = "Example@promisesJs.com") => {
     const name = document.getElementById("name");
-    name.innerHTML = nameRandom;
+    name.innerHTML = `<b>${nameRandom}</b>
+    ${email}`;
 };
